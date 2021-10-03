1e591cefa2c7a85d5ca906e09de20804a7654dfd
BulamuDilan <76627010+BulamuDilan@users.noreply.github.com>
2021-10-04 00:13:02 +0300

    procjet1

---
+++ CLionProjects/untitled1/cmake-build-debug/CMakeFiles/untitled1.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/dilanbulamu/CLionProjects/untitled1/container.cpp" "/Users/dilanbulamu/CLionProjects/untitled1/cmake-build-debug/CMakeFiles/untitled1.dir/container.cpp.o"
+  "/Users/dilanbulamu/CLionProjects/untitled1/main.cpp" "/Users/dilanbulamu/CLionProjects/untitled1/cmake-build-debug/CMakeFiles/untitled1.dir/main.cpp.o"
+  "/Users/dilanbulamu/CLionProjects/untitled1/number_types/complex.cpp" "/Users/dilanbulamu/CLionProjects/untitled1/cmake-build-debug/CMakeFiles/untitled1.dir/number_types/complex.cpp.o"
+  "/Users/dilanbulamu/CLionProjects/untitled1/number_types/fraction.cpp" "/Users/dilanbulamu/CLionProjects/untitled1/cmake-build-debug/CMakeFiles/untitled1.dir/number_types/fraction.cpp.o"
+  "/Users/dilanbulamu/CLionProjects/untitled1/number_types/number.cpp" "/Users/dilanbulamu/CLionProjects/untitled1/cmake-build-debug/CMakeFiles/untitled1.dir/number_types/number.cpp.o"
+  "/Users/dilanbulamu/CLionProjects/untitled1/number_types/point.cpp" "/Users/dilanbulamu/CLionProjects/untitled1/cmake-build-debug/CMakeFiles/untitled1.dir/number_types/point.cpp.o"
+  "/Users/dilanbulamu/CLionProjects/untitled1/number_types/polar.cpp" "/Users/dilanbulamu/CLionProjects/untitled1/cmake-build-debug/CMakeFiles/untitled1.dir/number_types/polar.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CLionProjects/untitled1/cmake-build-debug/CMakeFiles/untitled1.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled1.dir/container.cpp.o"
+  "CMakeFiles/untitled1.dir/main.cpp.o"
+  "CMakeFiles/untitled1.dir/number_types/complex.cpp.o"
+  "CMakeFiles/untitled1.dir/number_types/fraction.cpp.o"
+  "CMakeFiles/untitled1.dir/number_types/number.cpp.o"
+  "CMakeFiles/untitled1.dir/number_types/point.cpp.o"
+  "CMakeFiles/untitled1.dir/number_types/polar.cpp.o"
+  "untitled1"
+  "untitled1.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
